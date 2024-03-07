ac384c3986f30e2c4b5b8f53897a91883c85fdd7
Ondrej Sika <ondrej@ondrejsika.com>
2024-03-07 19:14:09 +0100

    feat(examples/go): Create simple Go example

---
+++ examples/go/go.mod
@@ -0,0 +1,21 @@
+module example
+
+go 1.21.6
+
+require (
+	github.com/coreos/go-oidc v2.2.1+incompatible
+	github.com/spf13/cobra v1.8.0
+	golang.org/x/oauth2 v0.18.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/pquerna/cachecontrol v0.2.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/crypto v0.21.0 // indirect
+	golang.org/x/net v0.22.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+	gopkg.in/square/go-jose.v2 v2.6.0 // indirect
+)
---
+++ examples/go/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/coreos/go-oidc"
+	"github.com/spf13/cobra"
+	"golang.org/x/oauth2"
+)
+
+var FlagIssuer string
+var FlagClientID string
+var FlagClientSecret string
+
+var Cmd = &cobra.Command{
+	Use:   "example",
+	Short: "Examle OIDC",
+	Run: func(cmd *cobra.Command, args []string) {
+		Server(FlagIssuer, FlagClientID, FlagClientSecret)
+	},
+}
+
+func init() {
+	Cmd.Flags().StringVar(
+		&FlagIssuer,
+		"issuer",
+		"",
+		"Issuer",
+	)
+	Cmd.MarkFlagRequired("issuer")
+	Cmd.Flags().StringVar(
+		&FlagClientID,
+		"client-id",
+		"",
+		"Client ID",
+	)
+	Cmd.MarkFlagRequired("client-id")
+	Cmd.Flags().StringVar(
+		&FlagClientSecret,
+		"client-secret",
+		"",
+		"Client Secret",
+	)
+	Cmd.MarkFlagRequired("client-secret")
+}
+
+func main() {
+	Cmd.Execute()
+}
+
+func Server(issuer, clientID, clientSecret string) {
+	ctx := context.Background()
+
+	provider, err := oidc.NewProvider(ctx, issuer)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	config := oauth2.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		Endpoint:     provider.Endpoint(),
+		RedirectURL:  "http://localhost:8080/callback",
+		Scopes:       []string{oidc.ScopeOpenID, "profile", "email"},
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, config.AuthCodeURL("state"), http.StatusFound)
+	})
+
+	http.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Query().Get("state") != "state" {
+			http.Error(w, "state did not match", http.StatusBadRequest)
+			return
+		}
+
+		oauth2Token, err := config.Exchange(ctx, r.URL.Query().Get("code"))
+		if err != nil {
+			http.Error(w, "Failed to exchange token: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		rawIDToken, ok := oauth2Token.Extra("id_token").(string)
+		if !ok {
+			http.Error(w, "No id_token field in oauth2 token.", http.StatusInternalServerError)
+			return
+		}
+
+		idToken, err := provider.Verifier(&oidc.Config{ClientID: clientID}).Verify(ctx, rawIDToken)
+		if err != nil {
+			http.Error(w, "Failed to verify ID Token: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		_ = idToken // ID Token is now verified and can be used
+
+		fmt.Println(rawIDToken)
+
+		fmt.Fprintf(w, "Login successful! %s", rawIDToken)
+	})
+
+	fmt.Println("http://localhost:8080/")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
