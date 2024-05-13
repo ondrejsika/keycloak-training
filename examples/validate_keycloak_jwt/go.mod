c0c7a377619b69ef82ae9769dfb0d3adb40d3f96
Ondrej Sika <ondrej@ondrejsika.com>
2024-05-13 09:06:09 +0200

    feat(examples/validate_keycloak_jwt): Add simple JWT validation example

---
+++ examples/validate_keycloak_jwt/go.mod
@@ -0,0 +1,12 @@
+module validate_keycloak_jwt
+
+go 1.22.0
+
+require github.com/coreos/go-oidc v2.2.1+incompatible
+
+require (
+	github.com/pquerna/cachecontrol v0.2.0 // indirect
+	golang.org/x/crypto v0.23.0 // indirect
+	golang.org/x/oauth2 v0.20.0 // indirect
+	gopkg.in/square/go-jose.v2 v2.6.0 // indirect
+)
---
+++ examples/validate_keycloak_jwt/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/coreos/go-oidc"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		log.Fatalf("Usage: %s <issuer> <rawIDToken>", os.Args[0])
+	}
+
+	issuer := os.Args[1]
+	rawIDToken := os.Args[2]
+
+	ctx := context.Background()
+
+	provider, err := oidc.NewProvider(ctx, issuer)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = provider.Verifier(&oidc.Config{SkipClientIDCheck: true}).Verify(ctx, rawIDToken)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("ID Token is valid")
+}
