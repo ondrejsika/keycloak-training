f225fa0c2703181485d2108b3f3d483b80ae7455
Ondrej Sika <ondrej@ondrejsika.com>
2025-02-25 09:47:58 +0100

    feat(examples/curl): Add simple example get_access_and_id_token.sh

---
+++ examples/curl/get_access_and_id_token.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+RESPONSE=$(curl -fsSL -X POST "https://sso.sikalabs.com/realms/training/protocol/openid-connect/token" \
+  -H "Content-Type: application/x-www-form-urlencoded" \
+  -d "client_id=example_client_id" \
+  -d "client_secret=example_client_secret" \
+  -d "grant_type=password" \
+  -d "scope=openid" \
+  -d "username=example_username" \
+  -d "password=example_password" )
+
+echo RAW RESPONSE:
+echo $RESPONSE | jq
+echo
